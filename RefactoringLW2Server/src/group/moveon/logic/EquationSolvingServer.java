691dd8af5464be85fe3f454aa9d1182b86cbef72
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-10-31 16:36:36 +0300

    Add files via upload

--- RefactoringLW2Server/src/group/moveon/logic/EquationSolvingServer.java
+++ RefactoringLW2Server/src/group/moveon/logic/EquationSolvingServer.java
@@ -3,17 +3,34 @@ package group.moveon.logic;
 import group.moveon.equations.LinearEquation;
 import group.moveon.equations.SquareEquation;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.rmi.RemoteException;
 
 public class EquationSolvingServer implements EquationSolver {
 
+    final String fileAddress = "C:\\Users\\Mikhail\\IdeaProjects\\RefactoringLW2Server\\src\\eqRegistry.txt";
+
     @Override
     public int[] solveEquation(int equationType, int[] params) throws RemoteException {
         if (equationType == 1) {
+
             LinearEquation le = new LinearEquation(params[0], params[1]);
             le.setResult(-le.getB()/le.getK());
             int[] res = new int[1];
             res[0] = le.getResult();
+
+
+            try (FileWriter writer = new FileWriter(fileAddress, false)) {
+                String line = equationType + " " + params[0] + " " + params[1] + " " + res[0];
+                writer.write(line);
+                writer.append('\n');
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println("An error while reading file occurred!");
+            }
+
+
             return res;
         } else if (equationType == 2) {
             SquareEquation se = new SquareEquation(params[0], params[1], params[2]);
@@ -21,6 +38,19 @@ public class EquationSolvingServer implements EquationSolver {
             int[] res = new int[2];
             res[0] = se.getResult()[0];
             res[1] = se.getResult()[1];
+
+
+            try (FileWriter writer = new FileWriter(fileAddress, false)) {
+                String line = equationType + " " + params[0] + " " + params[1] + " " + params[2] + " "
+                        + res[0] + " " + res[1];
+                writer.write(line);
+                writer.append('\n');
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println("An error while reading file occurred!");
+            }
+
+
             return res;
         } else {
             return new int[3];
